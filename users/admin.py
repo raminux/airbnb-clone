c50b02c5a63bf351ca291a66b6899ffc36d1982f
raminux <ramin.esmzad@hotmail.com>
2022-03-20 16:47:51 -0400

    Custom user model creation

---
+++ users/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from . import models
+
+
+@admin.register(models.User)
+class CustomUserAdmin(admin.ModelAdmin):
+
+    """Custom User Admin"""
+
+    list_display = ("username", "gender", "language", "currency", "superhost")
+    list_filter = (
+        "language",
+        "currency",
+        "superhost",
+    )
---
+++ users/models.py
@@ -0,0 +1,62 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+
+    """Custom User Model"""
+
+    GENDER_MALE = "male"
+    GENDER_FEMALE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),
+        (GENDER_FEMALE, "Female"),
+        (GENDER_OTHER, "Other"),
+    )
+
+    LANGUAGE_ENGLISH = "EN"
+    LANGUAGE_KOREAN = "KR"
+    LANGUAGE_PERSIAN = "FR"
+    LANGUAGE_TURKISH = "TR"
+
+    LANGUAGE_CHOICES = (
+        (LANGUAGE_ENGLISH, "English"),
+        (LANGUAGE_KOREAN, "Korean"),
+        (LANGUAGE_TURKISH, "Turkish"),
+        (LANGUAGE_PERSIAN, "Persian"),
+    )
+
+    CURRENCY_USD = "usd"
+    CURRENCY_KRW = "krw"
+    CURRENCY_IRR = "irr"
+    CURRENCY_TRL = "trl"
+
+    CURRENCY_CHOICES = (
+        (CURRENCY_USD, "USD"),
+        (CURRENCY_IRR, "IRR"),
+        (CURRENCY_KRW, "KRW"),
+        (CURRENCY_TRL, "TRL"),
+    )
+
+    avatar = models.ImageField(null=True, blank=True)
+    gender = models.CharField(
+        choices=GENDER_CHOICES,
+        max_length=10,
+        null=True,
+        blank=True,
+        default=GENDER_OTHER,
+    )
+    bio = models.TextField(
+        default="",
+        blank=True,
+    )
+    birthdate = models.DateField(null=True, blank=True)
+    language = models.CharField(
+        choices=LANGUAGE_CHOICES, max_length=3, null=True, blank=True
+    )
+    currency = models.CharField(
+        choices=CURRENCY_CHOICES, max_length=4, null=True, blank=True
+    )
+    superhost = models.BooleanField(default=False)
