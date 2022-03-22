ea3fae3628b1ebfa4cd070fdc2b048485bed3aa0
raminux <ramin.esmzad@hotmail.com>
2022-03-23 00:40:13 +0430

    Completing Room Model

--- config/settings.py
+++ config/settings.py
@@ -45,7 +45,11 @@ PROJECT_APPS = [
     "rooms.apps.RoomsConfig",
 ]
 
-INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS
+THIRD_PARTY_APPS = [
+    "django_countries",
+]
+
+INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
 
 
 MIDDLEWARE = [
