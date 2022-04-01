1b15b4f00e4831beb3782f790ad6ec703cfb9e23
raminux <ramin.esmzad@hotmail.com>
2022-04-01 22:38:04 +0430

    Using DetailView class for Room detail

--- rooms/views.py
+++ rooms/views.py
@@ -1,7 +1,5 @@
 from django.utils import timezone
-from django.http import Http404
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from . import models
 
 
@@ -22,9 +20,10 @@ class HomeView(ListView):
         return context
 
 
-def room_detail(request, pk):
-    try:
-        room = models.Room.objects.get(pk=pk)
-        return render(request, "rooms/detail.html", {"room": room})
-    except models.Room.DoesNotExist:
-        raise Http404()
+class RoomDetail(DetailView):
+
+    """Room Detail Definition"""
+
+    model = models.Room
+    pk_url_kwarg = "pk"
+    template_name = "rooms/room_detail.html"
