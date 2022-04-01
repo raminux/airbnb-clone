3901471bcf9bfa37107a1ea2e888b203fdc69c8f
raminux <ramin.esmzad@hotmail.com>
2022-04-01 12:20:01 -0400

    model get_absolute_url usage

--- rooms/models.py
+++ rooms/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django_countries.fields import CountryField
 from core import models as core_models
 
@@ -90,6 +91,9 @@ class Room(core_models.TimeStampedModel):
 
     def __str__(self):
         return self.name
+
+    def get_absolute_url(self):
+        return reverse("rooms:detail", kwargs={"pk": self.pk})
 
     def save(self, *args, **kwargs):
         self.city = str.capitalize(self.city)
