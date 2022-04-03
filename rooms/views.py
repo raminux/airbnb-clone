949d0a899e843ded90218ee88c06e076a781c012
raminux <ramin.esmzad@hotmail.com>
2022-04-03 09:57:23 -0500

    Starting to add Room search view

--- rooms/views.py
+++ rooms/views.py
@@ -1,4 +1,5 @@
 from django.utils import timezone
+from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from . import models
 
@@ -27,3 +28,9 @@ class RoomDetail(DetailView):
     model = models.Room
     pk_url_kwarg = "pk"
     template_name = "rooms/room_detail.html"
+
+
+def search(request):
+    city = request.GET.get("city")
+    city = str.capitalize(city)
+    return render(request, "rooms/search.html", {"city": city})
