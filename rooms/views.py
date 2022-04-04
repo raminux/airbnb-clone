fb596b4d347c1c572adc162ea490a55090cd8e63
raminux <ramin.esmzad@hotmail.com>
2022-04-04 16:35:48 +0430

    Finished Room search form

--- rooms/views.py
+++ rooms/views.py
@@ -36,15 +36,41 @@ def search(request):
     city = str.capitalize(city)
     selected_country = request.GET.get("country", "IR")
     selected_room_type = int(request.GET.get("room_type", 0))
-    room_types = models.RoomType.objects.all()
+    selected_amenities = request.GET.getlist("amenities")
+    selected_facilities = request.GET.getlist("facilities")
+    selected_instant = request.GET.get("instant", False)
+    selected_super_host = request.GET.get("super_host", False)
+
+    price = int(request.GET.get("price", 0))
+    beds = int(request.GET.get("beds", 0))
+    guests = int(request.GET.get("guests", 0))
+    bedrooms = int(request.GET.get("bedrooms", 0))
+    baths = int(request.GET.get("baths", 0))
+
     form = {
         "city": city,
         "selected_country": selected_country,
         "selected_room_type": selected_room_type,
+        "price": price,
+        "beds": beds,
+        "guests": guests,
+        "bedrooms": bedrooms,
+        "baths": baths,
+        "selected_amenities": selected_amenities,
+        "selected_facilities": selected_facilities,
+        "selected_instant": selected_instant,
+        "selected_super_host": selected_super_host,
     }
-    choices = {"countries": countries, "room_types": room_types}
-    test = {**form, **choices}
-    print(f"**form, **choices = {test}")
+
+    room_types = models.RoomType.objects.all()
+    amenities = models.Amenity.objects.all()
+    facilities = models.Facility.objects.all()
+    choices = {
+        "countries": countries,
+        "room_types": room_types,
+        "amenities": amenities,
+        "facilities": facilities,
+    }
     return render(
         request,
         "rooms/search.html",
