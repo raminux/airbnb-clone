51d93e3cc749fa255b69d09fc81e645404f06b01
raminux <ramin.esmzad@hotmail.com>
2022-04-05 10:17:50 +0430

    Filtering rooms manually like a Guru

--- rooms/views.py
+++ rooms/views.py
@@ -38,8 +38,8 @@ def search(request):
     selected_room_type = int(request.GET.get("room_type", 0))
     selected_amenities = request.GET.getlist("amenities")
     selected_facilities = request.GET.getlist("facilities")
-    selected_instant = request.GET.get("instant", False)
-    selected_super_host = request.GET.get("super_host", False)
+    selected_instant = bool(request.GET.get("instant", False))
+    selected_super_host = bool(request.GET.get("super_host", False))
 
     price = int(request.GET.get("price", 0))
     beds = int(request.GET.get("beds", 0))
@@ -71,8 +71,56 @@ def search(request):
         "amenities": amenities,
         "facilities": facilities,
     }
+
+    filter_args = {}
+
+    if city != "Anywhere":
+        filter_args["city__startswith"] = city
+
+    filter_args["country"] = selected_country
+
+    if selected_room_type != 0:
+        filter_args["room_type__pk"] = selected_room_type
+
+    if price != 0:
+        filter_args["price__lte"] = price
+
+    if beds != 0:
+        filter_args["beds__gte"] = beds
+
+    if guests != 0:
+        filter_args["guests__gte"] = guests
+
+    if bedrooms != 0:
+        filter_args["bedrooms__gte"] = bedrooms
+
+    if baths != 0:
+        filter_args["baths__gte"] = baths
+
+    if selected_instant is True:
+        filter_args["instant_book"] = True
+
+    if selected_super_host is True:
+        filter_args["host__superhost"] = True
+
+    rooms = models.Room.objects.filter(**filter_args)
+
+    if len(selected_amenities) > 0:
+        for s_amenity in selected_amenities:
+            rooms = rooms.filter(amenities__pk=int(s_amenity))
+
+    if len(selected_facilities) > 0:
+        for s_facility in selected_facilities:
+            rooms = rooms.filter(facilities__pk=int(s_facility))
+
+    print(filter_args)
+
     return render(
         request,
         "rooms/search.html",
-        {**form, **choices},
+        {
+            **form,
+            **choices,
+            "rooms": rooms,
+        },
     )
